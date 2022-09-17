cd7c833ff9f3871b6228f646254f2609fc8a0cd1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-09-17 16:45:38 -0300

    styling cards

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,48 @@ function App() {
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo </span>está aqui.</h1>
 
       <div className='grid grid-cols-6 gap-6'>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-1.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
+
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-2.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-3.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-4.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-5.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-6.png"/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anuncios</span>
+          </div>
         </a>
       </div>
     </div>
