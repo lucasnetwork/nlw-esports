ce828d23c41b34d542a24ed525486ffa7b7c6a29
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-09-20 11:45:58 -0300

    add fonts and loading components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,32 @@
-import {Background} from './src/components/Background'
-import {StatusBar} from 'react-native'
+import { Background } from "./src/components/Background";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+  
+
   return (
-    <Background >
-      <StatusBar barStyle="light-content" translucent backgroundColor="transparent"/>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoader ? <Home/>:<Loading/>}
     </Background>
   );
 }
- 
