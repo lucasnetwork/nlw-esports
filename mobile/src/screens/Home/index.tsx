d7a229e3169c2524a56b0fed1d4c5e90f92e8801
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-09-21 10:11:02 -0300

    create Heading component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
-import {View} from 'react-native'
+import {Image, View} from 'react-native'
 import {styles} from './styles'
+import logoImg from '../../assets/logo.png'
 
 export function Home(){
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <Image source={logoImg} style={styles.logo}/>
+        </View>
     )
 }
