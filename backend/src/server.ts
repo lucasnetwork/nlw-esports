66094c936f5aa1fc12c9e4d65a4dfb2116ed9a71
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-09-24 12:06:25 -0300

    Initial backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,3 +2,18 @@ import express from 'express'
 
 const app = express()
 
+app.get('/games',(req,res)=>{
+    return res.json([])
+})
+
+app.post('/ads',(req,res)=>{
+    return res.status(201).json([])
+})
+
+app.get('/games/:id/ads',(req,res)=>{
+    return res.json([])
+})
+
+app.get('ads/:id/discord')
+
+app.listen(3333)
